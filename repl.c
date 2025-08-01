5c221ab866fc44a75b41d8ce2c9747bcfc7a6f37
Abhigyan <314abh@gmail.com>
2025-08-02 03:29:36 +0530

    Introduce dictionary/string collission test
    Introduced string collision test and a REPL to quickly compute hashes.
    Tested against top 10k Googled words. Found no collisions. TODO: test
    against bigger wordlists/dictionaries.

---
+++ repl.c
@@ -0,0 +1,55 @@
+/* -- repl.c
+ * A REPL to compute hashes of strings.
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2025 Abhigyan <nourr@duck.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define BUFF_SIZE 4096
+
+extern void ah1(void *bytes, size_t size, uint32_t hash[4]);
+extern void ah1_print(uint32_t hash[]);
+
+int main(int argc, char **argv)
+{
+  uint32_t hash[4];
+  char buff[4096] = { 0 };
+
+  while (true) {
+    printf(">> ");
+    
+    fgets(buff, BUFF_SIZE, stdin);
+    ah1(buff, strlen(buff), hash);
+    ah1_print(hash);
+  }
+
+  return EXIT_SUCCESS;
+}
+
---
+++ test_dict.c
@@ -0,0 +1,134 @@
+/* -- test_hash.c
+ * Utility program to test AH1 hash function for hash collisions
+ * against word lists and dictionaries to measure performance for
+ * common English word inputs.
+ *
+ * MIT License
+ * 
+ * Copyright (c) 2025 Abhigyan <nourr@duck.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/* define max reading limits */
+#define MAX_LINE_LENGTH 1024
+
+extern void ah1(void *restrict bytes, size_t size, uint32_t hash[4]);
+extern void ah1_print(uint32_t hash[4]);
+
+typedef struct TestWord
+{
+  char *word;
+  uint32_t hash[4];
+} TestWord;
+
+char *get_word(FILE *file)
+{
+  char buff[MAX_LINE_LENGTH];
+  if (!fgets(buff, MAX_LINE_LENGTH, file)) {
+    perror("Unable to read word from file.");
+    exit(-1);
+  }
+
+  char *word = malloc(strlen(buff) + 1);
+  if (!word) {
+    perror("Unable to allocate memory to store word.");
+    exit(-1);
+  }
+
+  strcpy(word, buff);
+  return word;
+}
+
+unsigned int line_count(FILE *file)
+{
+  unsigned int lines = 0;
+  char c = getc(file);
+  while (c != EOF) {
+    if (c == '\n') lines++;
+    c = getc(file);
+  }
+
+  rewind(file);
+  return lines;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    printf("Usage: test_dict [FILE NAME]\n");
+    return -1;
+  }
+
+  FILE *wordlist = fopen(argv[1], "r");
+  if (!wordlist) {
+    perror("Unable to open given file.");
+    return -1;
+  }
+  
+  unsigned int lines = line_count(wordlist);
+  TestWord *tests = calloc(lines, sizeof(TestWord));
+  TestWord *test  = tests;
+
+  for (size_t i = 0; i < lines; ++i) {
+    test->word = get_word(wordlist);
+    ah1(test->word, strlen(test->word), test->hash);
+    uint32_t *h1 = test->hash;
+    
+    // printf("TEST CASE #%d\n", i + 1);
+    // printf("  SUBJECT: %s", test->word);
+    // printf("  ");
+    // ah1_print(test->hash);
+
+    for (int j = 0; j < i; ++j) {
+      TestWord *match = tests;
+      ah1(match->word, strlen(match->word), match->hash);
+
+      uint32_t *h2 = match->hash;
+
+      // printf("  TARGET:  %s", match->word);
+      // printf("  ");
+      // ah1_print(match->hash);
+
+
+      bool collides = ((h1[0] == h2[0]) & (h1[1] == h2[1]) & (h1[2] == h2[2]) & (h1[3] == h2[3]));
+      if (collides) {
+        printf("MATCH FOUND\n");
+        printf("  %s", test->word);
+        ah1_print(test->hash);
+        printf("  %s", match->word);
+        ah1_print(match->hash);
+        return 0;
+      }
+      match++;
+    }
+
+    test++;
+  }
+
+  return 0;
+}
+
+
